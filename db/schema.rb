1b5e78294017bedeb9b27ab4bf09dc690f9e7f99
NEHA SUWAl <neha@jyaasa.com>
2015-03-23 16:57:25 +0545

    Ran db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150323102251) do
+ActiveRecord::Schema.define(version: 20150323110932) do
 
   create_table "uploads", force: true do |t|
     t.integer  "user_id"
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "file"
   end
 
   create_table "users", force: true do |t|
