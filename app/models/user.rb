e7723b9e7d8f896dd5cb43c2a73638da3e079cfd
NEHA SUWAl <neha@jyaasa.com>
2015-03-23 16:12:36 +0545

    Added has_and_belongs_to_many uploads

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_many :my_uploads, class_name: "Uploads"
+  has_and_belongs_to_many :uploads
+
   def password_required?
     super if confirmed?
   end
