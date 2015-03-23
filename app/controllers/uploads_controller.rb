7baf26f9b19a7cafebf62fc08300fafe2e79a637
NEHA SUWAl <neha@jyaasa.com>
2015-03-23 17:26:15 +0545

    Added action index , new and create

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,3 +1,27 @@
 class UploadsController < ApplicationController
   before_action :authenticate_user!
+
+  def index
+    @uploads = current_user.uploads
+  end
+
+  def new
+    @uploads = current_user.my_uploads.new
+  end
+
+  def create
+    binding.pry
+    @uploads = current_user.my_uploads.create(cv_params)
+    if @uploads.save
+      flash[:notice] = "The file is uploaded."
+    else
+      flash[:notice] = "File is not uploaded."
+    end
+    redirect_to(root_path)
+  end
+
+  private
+  def cv_params
+    params.require(:upload).permit(:name ,:file ,:user_ids => [])
+  end
 end
