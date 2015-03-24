10334e80254e39fd1440a3bb04bb1763373fdaca
NEHA SUWAl <neha@jyaasa.com>
2015-03-24 11:47:08 +0545

    After the modification of user_id schema is changed

--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150324044430) do
+ActiveRecord::Schema.define(version: 20150324060037) do
 
   create_table "shares", force: true do |t|
     t.string "share_to"
   end
 
   create_table "uploads", force: true do |t|
-    t.integer  "user_id"
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "file"
+    t.text     "user_id"
   end
 
   create_table "users", force: true do |t|
