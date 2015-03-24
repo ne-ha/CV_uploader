2cfd8e534716b3b94bdb96d668aff4e487c9da47
NEHA SUWAl <neha@jyaasa.com>
2015-03-24 17:44:11 +0545

    Written test for upload model

---
+++ spec/models/upload_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe Upload, type: :model do
+  describe "file" do
+    it "should be required" do
+      upload = Upload.new(file: nil)
+      upload.valid?
+      expect(upload).to be_invalid?
+    end
+  end
+end
