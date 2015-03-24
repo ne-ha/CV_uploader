0dc9e7dbff0b895219080cca3460a4fccd93496d
NEHA SUWAl <neha@jyaasa.com>
2015-03-24 16:52:33 +0545

    Added factory for upload

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -5,4 +5,9 @@ FactoryGirl.define do
     password_confirmation "aaaaaaaa"
     confirmed_at Time.now
   end
+
+  factory :upload do
+    name "Anisha Dangol"
+    file { Rack::Test::UploadedFile.new(File.join(Rails.root, 'public', 'uploads', 'upload', 'file', 'app-Sample_CVs.pdf'))}
+  end
 end
