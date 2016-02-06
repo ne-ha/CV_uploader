2b579eafa5d7ff8d10bac638e076074bc29a49b9
NEHA SUWAl <neha@jyaasa.com>
2016-02-06 15:36:47 +0545

    Donot

--- spec/features/upload_resume_feature_spec.rb
+++ spec/features/upload_resume_feature_spec.rb
@@ -16,13 +16,18 @@ describe "user", :type=> :feature, :js => true do
   end
 
   it "should be able to delete resume" do
+    sleep 2
     visit root_path
+    sleep 2
     click_link "Upload Resume"
+    sleep 2
     within(".modal-content") do
       fill_in 'upload[name]', :with => 'Usha Maharjan'
       attach_file('upload[file]',"#{Rails.root}/public/uploads/upload/file/app-Sample_CVs.pdf")
     end
+      sleep 2
       click_button "Upload"
+      sleep 2
       expect(page).to have_content "The resume is uploaded."
   end
 end
